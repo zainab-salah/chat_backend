8324db3601deda32633d296c0b01374e9134bdc1
Kainy <kainykawai@gmail.com>
2025-02-15 12:45:55 +0300

    feat: secure WebSockets with JWT authentication

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,31 +1,42 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.exceptions import DenyConnection
 from django.contrib.auth.models import User
-from .models import ChatRoom, Message
+from rest_framework_simplejwt.tokens import AccessToken
 from channels.db import database_sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
+        
+        token = self.scope['query_string'].decode('utf-8').split('=')[-1]
+        print(token)
 
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        await self.accept()
+        try:
+            access_token = AccessToken(token)
+            self.user = await database_sync_to_async(User.objects.get)(id=access_token['user_id'])
+            self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+            self.room_group_name = f"chat_{self.room_name}"
+
+            # Add user to group
+            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+            await self.accept()
+
+        except Exception:
+         
+            await self.close()
 
     async def disconnect(self, close_code):
+   
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data["message"]
-        username = data["username"]
-        room_name = data["room_name"]
+        username = self.user.username
 
-        user = await database_sync_to_async(User.objects.get)(username=username)
-        chatroom = await database_sync_to_async(ChatRoom.objects.get)(name=room_name)
 
-        new_message = await database_sync_to_async(Message.objects.create)(
-            user=user, chatroom=chatroom, content=message
+        await database_sync_to_async(Message.objects.create)(
+            user=self.user, chatroom=ChatRoom.objects.get(name=self.room_name), content=message
         )
 
         await self.channel_layer.group_send(
